8c7def9160b895cf5785cbcec566b4fcc2fa9174
Irina-Galaxy <Vorobieva.Irina77@yandex.ru>
2023-05-10 15:31:43 +0300

    Выполнла 2.13

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -3,24 +3,30 @@ import PointListView from '../view/point-list-view.js';
 import PointView from '../view/point-view.js';
 import PointEditView from '../view/point-edit-view.js';
 import {render} from '../render.js';
+import PointsModel from '../models/points-model.js';
 
 export default class BoardPresenter {
-
+  #pointsModel = new PointsModel();
+  #pointsData = null;
+  #offersData = null;
+  #destinationsData = null;
   pointListComponent = new PointListView();
+
 
   constructor({boardContainer}) {
     this.boardContainer = boardContainer;
   }
 
-  init() {
+  async init() {
+    this.#pointsData = await this.#pointsModel.getRoutePoints();
+    this.#offersData = await this.#pointsModel.getOffers();
+    this.#destinationsData = await this.#pointsModel.getDestinations();
     render(new SortView(), this.boardContainer);
     render(this.pointListComponent, this.boardContainer);
-    render(new PointEditView(), this.pointListComponent.getElement());
+    render(new PointEditView(this.#pointsData[2], this.#destinationsData, this.#offersData), this.pointListComponent.getElement());
 
-    for (let i = 0; i < 3; i++) {
-      render(new PointView(), this.pointListComponent.getElement());
-    }
-
-
+    this.#pointsData.forEach((point) => {
+      render(new PointView(point, this.#destinationsData.find((el) => el.id === point.destination),this.#offersData.find((el) => el.type === point.type)), this.pointListComponent.getElement());
+    });
   }
 }
--- src/view/point-view.js
+++ src/view/point-view.js
@@ -1,32 +1,34 @@
 import {createElement} from '../render.js';
 
-function createPointTemplate() {
+function createPointTemplate(point, destinatition, offers) {
   return (
     `<li class="trip-events__item">
     <div class="event">
       <time class="event__date" datetime="2019-03-18">MAR 18</time>
       <div class="event__type">
-        <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+        <img class="event__type-icon" width="42" height="42" src="img/icons/${point.type.toLowerCase()}.png" alt="Event type icon">
       </div>
-      <h3 class="event__title">Taxi Amsterdam</h3>
+      <h3 class="event__title">${destinatition.name}</h3>
       <div class="event__schedule">
         <p class="event__time">
-          <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+          <time class="event__start-time" datetime="${point.dateStart}">${point.dateStart}</time>
           &mdash;
-          <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+          <time class="event__end-time" datetime="${point.dateStop}">${point.dateStop}</time>
         </p>
         <p class="event__duration">30M</p>
       </div>
       <p class="event__price">
-        &euro;&nbsp;<span class="event__price-value">20</span>
+        &euro;&nbsp;<span class="event__price-value">${point.basePrice}</span>
       </p>
       <h4 class="visually-hidden">Offers:</h4>
       <ul class="event__selected-offers">
+      ${offers.offers.map((offer) => `
         <li class="event__offer">
-          <span class="event__offer-title">Order Uber</span>
+          <span class="event__offer-title">${offer.title}</span>
           &plus;&euro;&nbsp;
-          <span class="event__offer-price">20</span>
-        </li>
+          <span class="event__offer-price">${offer.price}</span>
+        </li>`).join('')}
+
       </ul>
       <button class="event__favorite-btn event__favorite-btn--active" type="button">
         <span class="visually-hidden">Add to favorite</span>
@@ -43,8 +45,14 @@ function createPointTemplate() {
 }
 
 export default class PointView {
+  constructor(point, destination, offers) {
+    this.point = point;
+    this.destination = destination;
+    this.offers = offers;
+  }
+
   getTemplate() {
-    return createPointTemplate();
+    return createPointTemplate(this.point, this.destination, this.offers);
   }
 
   getElement() {
